28b94165ddab8efb33935e45917fd8be64c43287
Documenter.jl <documenter@juliadocs.github.io>
2022-04-29 04:17:17 +0000

    build based on 381a767

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DarkMatterProfiles","category":"page"},{"location":"#DarkMatterProfiles","page":"Home","title":"DarkMatterProfiles","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DarkMatterProfiles.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DarkMatterProfiles]","category":"page"}]
+}
